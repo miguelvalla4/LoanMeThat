be763fb6c2ae1dfcb8e913fd2fe5c4b9dd5b0a18
Garzolio <miguelvalla4@gmail.com>
2023-11-12 14:05:09 +0100

    bad ddd implementation deleted

---
+++ src/Application/Actions/Game/GamesAction.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application\Actions\Game;
+
+use App\Application\Actions\Action;
+use Psr\Http\Message\ResponseInterface;
+
+class GamesAction extends Action
+{
+
+    protected function action(): ResponseInterface
+    {
+
+    }
+}
